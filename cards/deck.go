c7eb95222800dec7df9bc0334f15d13917dcb77c
Savitha Raghunathan <saveetha13@gmail.com>
2021-08-25 15:49:56 -0400

    Adding shuffle & file ops funcs

--- cards/deck.go
+++ cards/deck.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
 )
 
@@ -42,4 +44,22 @@ func (d deck) toString() string {
 func (d deck) write2File(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPos := rand.Intn(len(d) - 1)
+		d[i], d[newPos] = d[newPos], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,17 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	cards := newdeck()
-	fmt.Println("------- All Hands on Deck ---------")
+	//cards := newdeck()
+	//cards.write2File("mycards")
 	cards.print()
-	fmt.Println("---------- After deal ------------")
-	deal1, remainingDeal := deal(cards, 5)
-	deal1.print()
-	fmt.Println("--------- Remaining deck ----------")
-	remainingDeal.print()
 
-	fmt.Println(cards.toString())
+	cards.shuffle()
+	cards.print()
 }
