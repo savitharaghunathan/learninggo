a895d4e703b6f30f2f029bbd4ea02cb9fa04b6ff
Savitha Raghunathan <saveetha13@gmail.com>
2021-08-25 16:32:56 -0400

    Adding more tests to test file ops

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,12 +1,15 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	cards := newdeck()
 
 	if len(cards) != 56 {
-		t.Errorf("Expected deck length of 52, instead got %v", len(cards))
+		t.Errorf("Expected deck length of 56, instead got %v", len(cards))
 	}
 
 	if cards[0] != "Ace Spades" {
@@ -16,4 +19,18 @@ func TestNewDeck(t *testing.T) {
 	if cards[len(cards)-1] != "King Hearts" {
 		t.Errorf("Expected last card as King Hearts, but got %v", cards[len(cards)-1])
 	}
+}
+
+func TestWrites2FileAndReadFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newdeck()
+	deck.write2File("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 56 {
+		t.Errorf("Expected deck length of 56, instead got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
