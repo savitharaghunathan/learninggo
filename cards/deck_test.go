4fedd4dabd5471819d2cb457f99da44d360b5c52
Savitha Raghunathan <saveetha13@gmail.com>
2021-08-25 16:22:40 -0400

    Adding more tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(cards) != 56 {
 		t.Errorf("Expected deck length of 52, instead got %v", len(cards))
 	}
+
+	if cards[0] != "Ace Spades" {
+		t.Errorf("Expected first card as Ace Spaces, but got %v", cards[0])
+	}
+
+	if cards[len(cards)-1] != "King Hearts" {
+		t.Errorf("Expected last card as King Hearts, but got %v", cards[len(cards)-1])
+	}
 }
